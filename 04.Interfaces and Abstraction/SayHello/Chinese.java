9922f4397d5b6a9802aa1336c3bda84d71768398
andonowv <81648825+andonowv@users.noreply.github.com>
2022-03-20 01:52:45 +0200

    Add files via upload

---
+++ 04.Interfaces and Abstraction/SayHello/Chinese.java
@@ -0,0 +1,18 @@
+package SayHello;
+
+public class Chinese implements Person{
+    private String name;
+
+    public Chinese (String name){
+        this.name = name;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String sayHello(){
+        return "Djydjybydjy";
+    }
+}
