ab87ad44b23ba94de6257459b67ad48a8de86a3a
andonowv <81648825+andonowv@users.noreply.github.com>
2022-03-20 01:53:13 +0200

    Add files via upload

---
+++ 04.Interfaces and Abstraction/SayHelloExtend/Chinese.java
@@ -0,0 +1,11 @@
+package SayHelloExtend;
+
+public class Chinese extends BasePerson{
+    public Chinese (String name){
+        super(name);
+    }
+    @Override
+    public String sayHello(){
+        return "Djydjybydjy";
+    }
+}
